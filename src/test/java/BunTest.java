6540b95b6c124c9e93d464f99749038fd24f4b1f
Igor Zobov <zobov.i.v@yandex.ru>
2024-04-29 11:14:27 +0300

    Коммит_4 правки тестов и pom

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,27 +1,23 @@
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class BunTest {
 
-    @Mock
-    private Bun bun;
-
-    private String name;
-    private float price;
+    final String name = "black bun";
+    final float price = 100;
 
     @Test
     public void getNameTest() {
+        Bun bun = new Bun("black bun", 100);
         assertEquals(name, bun.getName());
     }
 
     @Test
     public void getPriceTest() {
+        Bun bun = new Bun("black bun", 100);
         assertEquals(price, bun.getPrice(), 0.0);
     }
 
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,27 +1,25 @@
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Ingredient;
 
 import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.SAUCE;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class IngredientTest {
 
-    @Mock
-    private Ingredient ingredient;
 
-    private String name;
-    private float price;
+    final String name = "hot sauce";
+    final float price = 100;
 
     @Test
     public void getPriceTest() {
+        Ingredient ingredient = new Ingredient(SAUCE, "hot sauce", 100);
         assertEquals(price, ingredient.getPrice(), 0.0);
     }
 
     @Test
     public void getNameTest() {
+        Ingredient ingredient = new Ingredient(SAUCE, "hot sauce", 100);
         assertEquals(name, ingredient.getName());
     }
 
