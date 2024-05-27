91983b3d72840c30f39a43948c653fa3b147ba7c
Yaroslav Dranishnikov <slik-klick@mail.ru>
2024-05-28 00:51:59 +0300

    add tests

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,18 @@
+
+
+import org.junit.Test;
+import praktikum.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTypeTest {
+    @Test
+    public void selectSauceIngredientFromEnumIsCorrect() {
+        assertEquals(IngredientType.valueOf("SAUCE"), IngredientType.SAUCE);
+    }
+
+    @Test
+    public void selectFillingIngredientFromEnumIsCorrect() {
+        assertEquals(IngredientType.valueOf("FILLING"), IngredientType.FILLING);
+    }
+}
