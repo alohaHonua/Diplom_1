4adb2496bd5e19c04ba445d1a70b0fa658752cac
Lily Khamdamova <l.a.kopyonkina@yandex.ru>
2024-06-02 15:58:56 +0500

    Jacoco and .gitignore

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,34 +1,57 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
 import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class BurgerTest {
+
+    @Mock
+    private Bun mockBun;
+
+    @Mock
+    private Ingredient mockSauce;
+
+    @Mock
+    private Ingredient mockFilling;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     // Тест для проверки добавления ингредиента в бургер
     @Test
     public void testAddIngredient() {
         Burger burger = new Burger();
-        Ingredient ketchup = new Ingredient(IngredientType.SAUCE, "ketchup", 50);
+        when(mockSauce.getName()).thenReturn("ketchup");
+        when(mockSauce.getPrice()).thenReturn(50f);
+        when(mockSauce.getType()).thenReturn(IngredientType.SAUCE);
 
-        burger.addIngredient(ketchup);
+        burger.addIngredient(mockSauce);
         List<Ingredient> ingredients = burger.ingredients;
 
-        assertTrue("Список ингредиентов должен содержать добавленный ингредиент", ingredients.contains(ketchup));
+        assertTrue("Список ингредиентов должен содержать добавленный ингредиент", ingredients.contains(mockSauce));
     }
 
     // Тест для проверки удаления ингредиента из бургера
     @Test
     public void testRemoveIngredient() {
         Burger burger = new Burger();
-        Ingredient sausage = new Ingredient(IngredientType.FILLING, "sausage", 300);
+        when(mockSauce.getName()).thenReturn("sausage");
+        when(mockSauce.getPrice()).thenReturn(300f);
+        when(mockSauce.getType()).thenReturn(IngredientType.FILLING);
 
-        burger.addIngredient(sausage);
+        burger.addIngredient(mockSauce);
         burger.removeIngredient(0);
         List<Ingredient> ingredients = burger.ingredients;
 
@@ -39,45 +62,64 @@ public class BurgerTest {
     @Test
     public void testMoveIngredient() {
         Burger burger = new Burger();
-        Ingredient sourCream = new Ingredient(IngredientType.SAUCE, "sour cream", 200);
-        Ingredient cutlet = new Ingredient(IngredientType.FILLING, "cutlet", 100);
+        when(mockSauce.getName()).thenReturn("sour cream");
+        when(mockSauce.getPrice()).thenReturn(200f);
+        when(mockSauce.getType()).thenReturn(IngredientType.SAUCE);
 
-        burger.addIngredient(sourCream);
-        burger.addIngredient(cutlet);
+        when(mockFilling.getName()).thenReturn("cutlet");
+        when(mockFilling.getPrice()).thenReturn(100f);
+        when(mockFilling.getType()).thenReturn(IngredientType.FILLING);
+
+        burger.addIngredient(mockSauce);
+        burger.addIngredient(mockFilling);
 
         burger.moveIngredient(0, 1);
         List<Ingredient> ingredients = burger.ingredients;
 
-        assertEquals("Первый ингредиент должен быть котлета", cutlet, ingredients.get(0));
-        assertEquals("Второй ингредиент должен быть сметана", sourCream, ingredients.get(1));
+        assertEquals("Первый ингредиент должен быть котлета", mockFilling, ingredients.get(0));
+        assertEquals("Второй ингредиент должен быть сметана", mockSauce, ingredients.get(1));
     }
 
     // Тест для проверки расчета цены бургера
     @Test
     public void testGetPrice() {
         Burger burger = new Burger();
-        Bun bun = new Bun("white bun", 100);
-        Ingredient hotSauce = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
-        Ingredient dinosaur = new Ingredient(IngredientType.FILLING, "dinosaur", 200);
+        when(mockBun.getName()).thenReturn("white bun");
+        when(mockBun.getPrice()).thenReturn(100f);
 
-        burger.setBuns(bun);
-        burger.addIngredient(hotSauce);
-        burger.addIngredient(dinosaur);
+        when(mockSauce.getName()).thenReturn("hot sauce");
+        when(mockSauce.getPrice()).thenReturn(100f);
+        when(mockSauce.getType()).thenReturn(IngredientType.SAUCE);
 
-        assertEquals("Цена бургера должна быть 400", 500, burger.getPrice(), 0);
+        when(mockFilling.getName()).thenReturn("dinosaur");
+        when(mockFilling.getPrice()).thenReturn(200f);
+        when(mockFilling.getType()).thenReturn(IngredientType.FILLING);
+
+        burger.setBuns(mockBun);
+        burger.addIngredient(mockSauce);
+        burger.addIngredient(mockFilling);
+
+        assertEquals("Цена бургера должна быть 500", 500, burger.getPrice(), 0);
     }
 
     // Тест для проверки создания чека бургера
     @Test
     public void testGetReceipt() {
         Burger burger = new Burger();
-        Bun bun = new Bun("black bun", 150);
-        Ingredient chiliSauce = new Ingredient(IngredientType.SAUCE, "chili sauce", 300);
-        Ingredient sausage = new Ingredient(IngredientType.FILLING, "sausage", 300);
+        when(mockBun.getName()).thenReturn("black bun");
+        when(mockBun.getPrice()).thenReturn(150f);
 
-        burger.setBuns(bun);
-        burger.addIngredient(chiliSauce);
-        burger.addIngredient(sausage);
+        when(mockSauce.getName()).thenReturn("chili sauce");
+        when(mockSauce.getPrice()).thenReturn(300f);
+        when(mockSauce.getType()).thenReturn(IngredientType.SAUCE);
+
+        when(mockFilling.getName()).thenReturn("sausage");
+        when(mockFilling.getPrice()).thenReturn(300f);
+        when(mockFilling.getType()).thenReturn(IngredientType.FILLING);
+
+        burger.setBuns(mockBun);
+        burger.addIngredient(mockSauce);
+        burger.addIngredient(mockFilling);
 
         String expectedReceipt = "(==== black bun ====)\n" +
                 "= sauce chili sauce =\n" +
