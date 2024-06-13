06ce0df7f7fe4da9bae196698777371565722442
Anton <157398457+Anton199621@users.noreply.github.com>
2024-06-13 13:04:43 +0300

    Add files via upload

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,20 @@
+package praktikum;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+    private Bun bun;
+    @Before
+    public void setup() {
+        bun = new Bun("Флюоресцентная булка R2-D3", 100);
+    }
+    @Test
+    public void testGetBunName() {
+        assertEquals("Флюоресцентная булка R2-D3",bun.getName());
+    }
+    @Test
+    public void testGetBunPrice() {
+        assertEquals(100, bun.getPrice(), 0);
+    }
+}
