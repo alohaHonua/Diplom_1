7461ec8edd396b815a1c8de091df6cc643b14f1b
Julia Verman <lilou@vrmn.ru>
2024-06-17 15:06:24 +0300

    Добавила тест getReceipt

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -46,4 +46,27 @@ public class BurgerTest {
         Mockito.when(ingredient.getPrice()).thenReturn(100F);
         assertEquals(500F, burger.getPrice(), 0.0f);
     }
+    @Test
+    public void getReceiptTest() {
+        StringBuilder receiptExpect = new StringBuilder();
+
+        Mockito.when(bun.getName()).thenReturn("Test");
+        Mockito.when(ingredient.getName()).thenReturn("Test");
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.FILLING);
+        Mockito.when(bun.getPrice()).thenReturn(100F);
+        Mockito.when(ingredient.getPrice()).thenReturn(100F);
+
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+
+        String header = String.format("(==== %s ====)%n", bun.getName());
+        String price = String.format("%nPrice: %f%n", burger.getPrice());
+        String ingredient = String.format("= %s %s =%n", IngredientType.FILLING.toString().toLowerCase(), "Test");
+
+        receiptExpect.append(header);
+        receiptExpect.append(ingredient);
+        receiptExpect.append(header);
+        receiptExpect.append(price);
+        assertEquals(receiptExpect.toString(), burger.getReceipt());
+    }
 }
