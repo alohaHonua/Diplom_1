fd9e36ca806e352c0cdf4728d8d62fc143875bc6
Maria Mandzhieva <manmariia@yandex.ru>
2024-07-13 15:03:04 +0300

    BunTest

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,23 @@
+package praktikum;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+    Bun bun = new Bun ("black bun", 100);
+
+    @Test
+    public void getNameTest(){
+        String actualName = bun.getName();
+        String expectedName = "black bun";
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void getPriceTest(){
+        float actualPrice = bun.getPrice();
+        float expectedPrice = 100;
+        assertEquals(expectedPrice, actualPrice, 0);
+    }
+}
