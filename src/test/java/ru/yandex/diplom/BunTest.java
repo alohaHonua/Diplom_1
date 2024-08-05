bf4fc0a9b4e3e418ddf712275bc702b53696da48
Mikhail Churikov <churikov-20@yandex.ru>
2024-08-05 20:47:31 +0300

    develop1 - refactor pom.xml and add first test class

---
+++ src/test/java/ru/yandex/diplom/BunTest.java
@@ -0,0 +1,34 @@
+package ru.yandex.diplom;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BunTest {
+
+    private Bun bun;
+
+    @Before
+    public void setUp() {
+        bun = new Bun("Биг мак", 289.0f);
+    }
+
+    @Test
+    public void shouldReturnTrueGetName() {
+        String actualName = bun.getName();
+        assertEquals("Биг мак", actualName);
+    }
+
+    @Test
+    public void shouldReturnTrueGetPrice() {
+        float actualPrice = bun.getPrice();
+        assertEquals(289.0f, actualPrice, 0.0);
+    }
+
+}
