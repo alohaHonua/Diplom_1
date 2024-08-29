6aac46757d084526be86727669db98025b5789f2
Semen Baranov <b4ranov.s.a@yandex.com>
2024-08-29 22:23:10 +0300

    -added db test and ingredients test

---
+++ src/test/java/DataBaseTest.java
@@ -0,0 +1,56 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+import praktikum.Bun;
+import praktikum.Database;
+import praktikum.Ingredient;
+
+import java.util.List;
+
+public class DataBaseTest {
+
+
+    @Mock
+    List<Bun> buns;
+    @Mock
+    List<Ingredient> ingredients;
+
+
+    @Test
+    public void availableBunsReturnsSameListOfBunsTest() {
+
+        //TODO(): Сделать лучше
+        Database database = new Database(buns, ingredients);
+
+        Assert.assertEquals(buns, database.availableBuns());
+
+    }
+
+    @Test
+    public void availableIngredientsReturnsSameListOfIngredientsTest() {
+
+        //TODO(): Сделать лучше
+        Database database = new Database(buns, ingredients);
+
+        Assert.assertEquals(ingredients, database.availableIngredients());
+
+    }
+
+    @Test
+    public void defaultDataBaseHasThreeBunsTest() {
+        Database database = new Database();
+
+        int expectedAmountOfBuns = 3;
+
+        Assert.assertEquals(database.availableBuns().size(), expectedAmountOfBuns);
+    }
+
+    @Test
+    public void defaultDataBaseHasSixIngredientsTest() {
+        Database database = new Database();
+
+        int expectedAmountOfIngredients = 6;
+
+        Assert.assertEquals(database.availableIngredients().size(), expectedAmountOfIngredients);
+    }
+}
