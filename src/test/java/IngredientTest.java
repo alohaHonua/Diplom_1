e6b6225b2a1517cf0a2fd8c8a99064c81142622e
Mukhammed Nurmukhanov <Muhammed.Nurmuhanov@mail.ru>
2024-10-14 14:18:41 +0600

    Added tests for Ingredient.java

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IngredientTest {
+
+    private Ingredient ingredient;
+    private IngredientType type;
+    private String name;
+    private float price;
+
+    public IngredientTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { IngredientType.SAUCE, "Ketchup", 50.0f },
+                { IngredientType.FILLING, "Cheese", 30.0f }
+        });
+    }
+
+    @Test
+    public void testIngredientCreation() {
+        ingredient = new Ingredient(type, name, price);
+        assertEquals(type, ingredient.getType());
+        assertEquals(name, ingredient.getName());
+        assertEquals(price, ingredient.getPrice(), 0.001f);
+    }
+}
