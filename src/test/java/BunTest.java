a0ab3c455c435c7cd554fe253a3e57588f6056c4
Bogdan Diakov <bogdanechavarria@yandex.ru>
2024-10-14 20:04:48 +0500

    commit BunTest and pom.xml

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import praktikum.Bun;
+
+public class BunTest {
+
+    private static final String BUN_NAME = "Cat";
+    private static final float BUN_PRICE = 1.5f;
+    private static final float DELTA = 0000.1f;
+
+    private Bun bun;
+
+    @Before
+    public void setUp() {
+        bun = new Bun(BUN_NAME, BUN_PRICE);
+    }
+
+    @Test
+    public void getNameTest() {
+        String actual = bun.getName();
+        Assert.assertEquals(BUN_NAME, actual);
+    }
+
+    @Test
+    public void getPriceTest() {
+        float actual = bun.getPrice();
+        Assert.assertEquals(BUN_PRICE, actual, DELTA);
+    }
+}
