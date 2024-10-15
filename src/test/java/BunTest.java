4080e398a889ee3f00208558a88105e18bcc5119
Bogdan Diakov <bogdanechavarria@yandex.ru>
2024-10-15 22:07:42 +0500

    Modify BurgerTest

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -5,26 +5,22 @@ import praktikum.Bun;
 
 public class BunTest {
 
-    private static final String BUN_NAME = "black bun";
-    private static final float BUN_PRICE = 1.9f;
-    private static final float DELTA = 0000.1f;
-
     private Bun bun;
 
     @Before
     public void setUp() {
-        bun = new Bun(BUN_NAME, BUN_PRICE);
+        bun = new Bun(Credentials.BUN_NAME, Credentials.BUN_PRICE);
     }
 
     @Test
     public void getNameTest() {
         String actual = bun.getName();
-        Assert.assertEquals(BUN_NAME, actual);
+        Assert.assertEquals(Credentials.BUN_NAME, actual);
     }
 
     @Test
     public void getPriceTest() {
         float actual = bun.getPrice();
-        Assert.assertEquals(BUN_PRICE, actual, DELTA);
+        Assert.assertEquals(Credentials.BUN_PRICE, actual, Credentials.DELTA);
     }
 }
--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -8,39 +8,71 @@ import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 
 public class BurgerTest {
 
-    private static final String BUN_NAME = "black bun";
-    private static final float BUN_PRICE = 1.9f;
-    private static final float DELTA = 0000.1f;
-
     @Mock
     Bun bun;
     @Mock
     Ingredient ingredient;
+    @Mock
+    List<Ingredient> ingredients;
 
     @Test
     public void setBunTest() {
-        Mockito.when(bun.getName()).thenReturn(BUN_NAME);
-        Mockito.when(bun.getPrice()).thenReturn(BUN_PRICE);
+        Mockito.when(bun.getName()).thenReturn(Credentials.BUN_NAME);
+        Mockito.when(bun.getPrice()).thenReturn(Credentials.BUN_PRICE);
         Burger burger = new Burger();
         burger.setBuns(bun);
-        Assert.assertEquals(BUN_NAME, bun.getName());
-        Assert.assertEquals(BUN_PRICE, bun.getPrice(), DELTA);
+        Assert.assertEquals(Credentials.BUN_NAME, bun.getName());
+        Assert.assertEquals(Credentials.BUN_PRICE, bun.getPrice(), Credentials.DELTA);
         Assert.assertEquals(bun, burger.bun);
     }
 
     @Test
     public void addIngredientTest() {
         Burger burger = new Burger();
-        List<Ingredient> ingredients = burger.ingredients;
+        burger.ingredients = ingredients;
         burger.addIngredient(ingredient);
-        Assert.assertEquals(1, ingredients.size());
-        Assert.assertEquals(ingredient, ingredients.get(0));
+        Mockito.verify(ingredients, Mockito.times(1)).add(ingredient);
     }
+
+    @Test
+    public void removeIngredientTest() {
+        Burger burger = new Burger();
+        burger.ingredients = ingredients;
+        burger.removeIngredient(Credentials.INDEX);
+        Mockito.verify(ingredients, Mockito.times(1)).remove(Credentials.INDEX);
+    }
+
+    @Test
+    public void moveIngredientTest() {
+        Burger burger = new Burger();
+        burger.ingredients = ingredients;
+        burger.moveIngredient(Credentials.INDEX, Credentials.NEW_INDEX);
+        Mockito.verify(ingredients, Mockito.times(1))
+                .add(Credentials.NEW_INDEX, ingredients.remove(Credentials.INDEX));
+    }
+
+    @Test
+    public void getPriceTest() {
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        Mockito.when(bun.getPrice()).thenReturn(Credentials.BUN_PRICE);
+        List<Ingredient> ingredients = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            ingredients.add(ingredient);
+        }
+        burger.ingredients = ingredients;
+        Mockito.when(ingredient.getPrice()).thenReturn(Credentials.INGREDIENT_PRICE);
+        burger.getPrice();
+        Mockito.verify(ingredient, Mockito.times(5)).getPrice();
+        Assert.assertEquals(Credentials.TOTAL_PRICE, burger.getPrice(), Credentials.DELTA);
+
+    }
+
 }
