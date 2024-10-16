a293203b902426f8924b17be430936b864276ba8
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-16 11:55:57 +0300

    Add TestForEnumIngredientType

---
+++ src/test/java/praktitum/TestForEnumIngredientType.java
@@ -0,0 +1,47 @@
+package praktitum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+
+//public class TestForEnumIngredientType {
+
+//    @Test
+//    public void testSauce() {
+//        assertEquals("SAUCE", IngredientType.SAUCE.name());
+//    }
+
+//    @Test
+//    public void testFilling() {
+//        assertEquals("FILLING", IngredientType.FILLING.name());
+//    }
+//}
+
+
+@RunWith(Parameterized.class)
+public class TestForEnumIngredientType {
+
+    private IngredientType ingredientType;
+    private String expectedIngredientTypeName;
+
+    public TestForEnumIngredientType(String expectedIngredientTypeName, IngredientType ingredientType) {
+        this.ingredientType = ingredientType;
+        this.expectedIngredientTypeName = expectedIngredientTypeName;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] ingredientTypeParameters() {
+        return new Object[][]{
+                {"SAUCE", IngredientType.SAUCE},
+                {"FILLING", IngredientType.FILLING}
+        };
+    }
+
+    @Test
+    public void testIngredientTypeName() {
+        assertEquals(expectedIngredientTypeName, ingredientType.name());
+    }
+}
