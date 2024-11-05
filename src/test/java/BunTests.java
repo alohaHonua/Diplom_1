a7394fb81523c8bc25fd6dffdd228c1db7f1438d
Kamila Adyukova <kamila.adyukova@yandex.ru>
2024-11-05 15:22:43 +0300

    test cases

--- src/test/java/BunTests.java
+++ src/test/java/BunTests.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class BunTests {
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BunTests {
+
+    String name = "bun";
+
+    @Test
+    public void testBunCreation() {
+        float price = 0.0005F;
+
+        Bun bun = new Bun(name, price);
+
+        assertNotNull(bun);
+        assertEquals(name, bun.getName());
+        assertEquals(price, bun.getPrice(), 0.0001f);
+    }
+
+    @Test
+    public void testGetName() {
+        Bun bun = new Bun(name, 0.2F);
+
+        String actualName = bun.getName();
+
+        assertEquals(name, actualName);
+    }
+
+    @Test
+    public void testGetPrice() {
+        float price = 0.1F;
+        Bun bun = new Bun("white bun", price);
+
+        float actualPrice = bun.getPrice();
+
+        assertEquals(0.01, actualPrice, 0.1);
+    }
 }
+
