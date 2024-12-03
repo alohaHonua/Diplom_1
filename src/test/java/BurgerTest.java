fbcc670053184c45f34a4f5dffce27cd0c4fb365
Mihail Drozhzhin <137402801+Kosolapkin@users.noreply.github.com>
2024-12-03 12:24:46 +0300

    Update BurgerTest.java

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -8,9 +8,6 @@ import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
