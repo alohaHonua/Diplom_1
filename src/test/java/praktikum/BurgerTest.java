b5c2bd35d2b3b9f739cbe83c6b70110706c0e536
Yana Shmygleva <shmyaniv@mail.ru>
2024-12-18 20:10:35 +0300

    18/12/2024: add Test classes for each class

---
+++ src/test/java/praktikum/BurgerTest.java
@@ -0,0 +1,42 @@
+package praktikum;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BurgerTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void setBuns() {
+  }
+
+  @Test
+  public void addIngredient() {
+  }
+
+  @Test
+  public void removeIngredient() {
+  }
+
+  @Test
+  public void moveIngredient() {
+  }
+
+  @Test
+  public void getPrice() {
+  }
+
+  @Test
+  public void getReceipt() {
+  }
+}
---
+++ src/test/java/praktikum/IngredientParametrizedTest.java
@@ -0,0 +1,28 @@
+package praktikum;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IngredientParametrizedTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void getPrice() {
+  }
+
+  @Test
+  public void getName() {
+  }
+
+  @Test
+  public void getType() {
+  }
+}
