17822a903e4f681d8913af4c209df95632151fb7
Yana Shmygleva <shmyaniv@mail.ru>
2024-12-18 20:26:26 +0300

    18/12/2024: add BunTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,26 +1,31 @@
 package praktikum;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BunTest {
+  private Bun bun;
 
   @Before
   public void setUp() throws Exception {
-  }
-
-  @After
-  public void tearDown() throws Exception {
+    bun = new Bun("brownie", 200.0f);
   }
 
   @Test
   public void getName() {
+    String expected = "brownie";
+    String actual = bun.getName();
+
+    assertEquals("Incorrect bun name", expected, actual);
   }
 
   @Test
   public void getPrice() {
+    float expected = 200.0f;
+    float actual = bun.getPrice();
+
+    assertEquals("Incorrect bun price", expected, actual, 0);
   }
 }
