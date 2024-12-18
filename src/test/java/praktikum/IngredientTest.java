71f77e7b2b4174a8cb214129501ef7fa46a48d20
Yana Shmygleva <shmyaniv@mail.ru>
2024-12-18 20:36:06 +0300

    18/12/2024: create IngredientTest

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,30 +1,32 @@
 package praktikum;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class IngredientTest {
+  private Ingredient ingredient;
 
   @Before
   public void setUp() throws Exception {
-  }
-
-  @After
-  public void tearDown() throws Exception {
+    ingredient = new Ingredient(IngredientType.SAUCE, "vassabi", 65.0f);
   }
 
   @Test
   public void getPrice() {
+    float expected = 65.0f;
+    float actual = ingredient.getPrice();
+
+    assertEquals("Incorrect ingredient price", expected, actual, 0);
   }
 
   @Test
   public void getName() {
+    String expected = "vassabi";
+    String actual = ingredient.getName();
+
+    assertEquals("Incorrect ingredient name", expected, actual);
   }
 
-  @Test
-  public void getType() {
-  }
 }
