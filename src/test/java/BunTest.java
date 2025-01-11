fb0f4f8ba6205e5ad5a2bb65c15ff9c790eb2e67
KatyaSadykova <ThisIsKaterin@yandex.ru>
2025-01-11 22:59:21 +0300

    Diplom1

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+
+public class BunTest {
+
+    public String name;
+    public float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"black bun", 100},
+                {"white bun", 200},
+                {"red bun", 300}
+        });
+    }
+
+    @Test
+    public void testBunModel() {
+        Bun bun = new Bun(name, price);
+        Assert.assertEquals(name, bun.getName());
+        Assert.assertEquals(price, bun.getPrice(), 0);
+    }
+}
