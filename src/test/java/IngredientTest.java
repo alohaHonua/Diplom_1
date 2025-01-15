41838d39467965e816adf3000ba58a4b6ca8a47b
Ilsina Komarova <ilsinchik@list.ru>
2025-01-15 16:59:46 +0300

    add IngredientTest

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,45 @@
+import io.qameta.allure.Step;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+@RunWith(Parameterized.class)
+public class IngredientTest {
+
+    private final IngredientType ingredientType;
+    private final String expectedName;
+    private final float expectedPrice;
+
+    public IngredientTest(IngredientType ingredientType, String expectedName, float expectedPrice) {
+        this.ingredientType = ingredientType;
+        this.expectedName = expectedName;
+        this.expectedPrice = expectedPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {IngredientType.SAUCE, "hot sauce", 100f},
+                {IngredientType.FILLING, "cutlet", 100f}
+        };
+    }
+
+    @Test
+    @Step("Тестирование ингредиента типа: {0}, с именем: {1} и ценой: {2}")
+    public void testIngredient() {
+        Ingredient ingredient = new Ingredient(ingredientType, expectedName, expectedPrice);
+
+        assertSoftly(softly -> {
+            softly.assertThat(ingredient.getName()).isEqualTo(expectedName);
+            softly.assertThat(ingredient.getPrice()).isEqualTo(expectedPrice);
+            softly.assertThat(ingredient.getType()).isEqualTo(ingredientType);
+        });
+    }
+}
+
+
+
