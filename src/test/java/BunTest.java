28718a6d70fe18557d543d6f52b48f742201d292
KatyaSadykova <ThisIsKaterin@yandex.ru>
2025-01-21 23:26:31 +0300

    Diplom-1

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.Collection;
 
 @RunWith(Parameterized.class)
-
 public class BunTest {
 
     public String name;
@@ -22,16 +21,26 @@ public class BunTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"black bun", 100},
-                {"white bun", 200},
-                {"red bun", 300}
+                {"black bun", 100},       // Позитивный тест
+                {"", 100},               // Пустое имя
+                {"a".repeat(256), 100},  // Длинное имя
+                {"white bun", 0},        // Нулевая цена
+                {"red bun", -50},        // Отрицательная цена
+                {"green bun", 10.5f},     // Положительная дробная цена
+                {"blue bun", 200}         // Позитивный тест
         });
     }
 
     @Test
-    public void testBunModel() {
+    public void testBunName() {
         Bun bun = new Bun(name, price);
-        Assert.assertEquals(name, bun.getName());
-        Assert.assertEquals(price, bun.getPrice(), 0);
+        Assert.assertEquals("Имя булочки не совпадает", name, bun.getName());
     }
+
+    @Test
+    public void testBunPrice() {
+        Bun bun = new Bun(name, price);
+        Assert.assertEquals("Цена булочки не совпадает", price, bun.getPrice(), 0);
+    }
+
 }
