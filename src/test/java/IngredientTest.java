587963be0b196d3ed5d83e839d5589807c6af563
Ermak0vYurii <grantovod1@gmail.com>
2025-01-24 12:52:47 +0500

    IngredientTest 100%

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,25 +1,49 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
 import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.*;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
 
-    private final Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
+    private final String name;
+    private final float price;
+    private final IngredientType type;
+
+    public IngredientTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] testData() {
+        return new Object[][]{
+                {SAUCE, "hot sauce", 100},
+                {FILLING, "cutlet", 200}
+        };
+    }
 
     @Test
     public void getPriceTest() {
-        assertEquals(100, ingredient.getPrice(), 0);
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(price, ingredient.getPrice(), 0);
     }
 
     @Test
     public void getNameTest() {
-        assertEquals("hot sauce", ingredient.getName());
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(name, ingredient.getName());
     }
 
     @Test
     public void getTypeTest() {
-        assertEquals(IngredientType.SAUCE, ingredient.getType());
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(type, ingredient.getType());
     }
 }
