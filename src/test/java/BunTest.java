520f7bc3bdcacb64ba1a72cf5ce82a7c24509425
Ermak0vYurii <grantovod1@gmail.com>
2025-01-24 09:44:58 +0500

    BunTest 100%

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -5,17 +5,15 @@ import static org.junit.Assert.assertEquals;
 
 public class BunTest {
 
-    private Bun bun;
+    private final Bun bun = new Bun("black bun", 100);
 
     @Test
     public void getNameTest() {
-         bun = new Bun("black bun", 100);
          assertEquals("black bun", bun.getName());
     }
 
     @Test
     public void getPriceTest() {
-        bun = new Bun("black bun", 100);
         assertEquals(100, bun.getPrice(), 0);
     }
 
