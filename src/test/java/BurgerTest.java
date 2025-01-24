ae16fb3716b00aebcb6d52921e6ffb5ed931da16
Ermak0vYurii <grantovod1@gmail.com>
2025-01-24 15:05:45 +0500

    BurgerTest 100%

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -6,9 +6,11 @@ import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 import static Constants.Constants.*;
 import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
@@ -58,7 +60,24 @@ public class BurgerTest {
         float burgerPrice = burger.getPrice();
         float expectedPrice = PRICE_BUN * 2 + PRICE_INGREDIENT;
         assertEquals("Стоимость бургера расчитана неверно", expectedPrice, burgerPrice, 0);
+    }
 
+    @Test
+    public void getReceiptTest() {
+        Mockito.when(bunMock.getName()).thenReturn(NAME_BUN);
+        Mockito.when(bunMock.getPrice()).thenReturn(PRICE_BUN);
+        Mockito.when(ingredientMock.getType()).thenReturn(FILLING);
+        Mockito.when(ingredientMock.getName()).thenReturn(NAME_INGREDIENT);
+        Mockito.when(ingredientMock.getPrice()).thenReturn(PRICE_INGREDIENT);
+        burger.setBuns(bunMock);
+        burger.addIngredient(ingredientMock);
+        String actualReceipt = burger.getReceipt();
+        String expectedReceipt = String.format("(==== %s ====)%n", NAME_BUN) +
+                String.format("= %s %s =%n", FILLING.toString().toLowerCase(), NAME_INGREDIENT) +
+                String.format("(==== %s ====)%n", NAME_BUN) +
+                String.format("%nPrice: %f%n", burger.getPrice());
+        System.out.println(expectedReceipt);
+        assertEquals(expectedReceipt, actualReceipt);
 
     }
 }
