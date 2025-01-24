b23df2786c139609eead48618308059ed1579c44
Ermak0vYurii <grantovod1@gmail.com>
2025-01-24 05:20:35 +0500

    IngredientTest 100%

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -6,23 +6,20 @@ import static org.junit.Assert.assertEquals;
 
 public class IngredientTest {
 
-    private Ingredient ingredient;
+    private final Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
 
     @Test
     public void getPriceTest() {
-        ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
         assertEquals(100, ingredient.getPrice(), 0.01);
     }
 
     @Test
     public void getNameTest() {
-        ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
         assertEquals("hot sauce", ingredient.getName());
     }
 
     @Test
     public void getTypeTest() {
-        ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
         assertEquals(IngredientType.SAUCE, ingredient.getType());
     }
 }
