967bbffdd895d8de7d8bb9ece2b288b10cf59d0f
Ermak0vYurii <grantovod1@gmail.com>
2025-01-24 15:05:20 +0500

    add Constants.java

--- src/test/java/Constants/Constants.java
+++ src/test/java/Constants/Constants.java
@@ -3,6 +3,6 @@ package Constants;
 public class Constants {
     public static final String NAME_BUN = "name bun";
     public static final float PRICE_BUN = 120F;
-    public static final String NAME_INGREDIENT = "name ingredient";
+    public static final String NAME_INGREDIENT = "ingredient";
     public static final float PRICE_INGREDIENT = 150F;
 }
