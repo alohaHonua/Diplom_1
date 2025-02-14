bf610cc9a3c63078e47ea89500fd96b063e29352
Vladimir Timofeev <v.timofeev1977@ya.ru>
2025-02-14 17:21:57 +0300

    delete MockTest, change BurgerTest

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,4 +1,9 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
 
@@ -6,7 +11,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static praktikum.IngredientType.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
+
+    @Mock
+    Bun bun;
 
     @Test
     public void checkAddIngridient() {
@@ -37,5 +46,19 @@ public class BurgerTest {
         Ingredient firstIngredient = burger.ingredients.get(0);
         burger.moveIngredient(0, 1);
         assertEquals(firstIngredient, burger.ingredients.get(1));
+    }
+
+    @Test
+    public void checkGetPrice() {
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        Mockito.when(burger.bun.getPrice()).thenReturn(50F);
+        Ingredient cucumberFilling = new Ingredient(FILLING, "cucumber", 1.5F);
+        Ingredient cheeseSauce = new Ingredient(SAUCE, "cheesy", 2.5F);
+        burger.addIngredient(cucumberFilling);
+        burger.addIngredient(cheeseSauce);
+        float actualsPrice = burger.getPrice();
+        float delta = 0.1F;
+        assertEquals(104, actualsPrice, delta);
     }
 }
