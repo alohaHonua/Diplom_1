d280061354194428d9a67f365c647eaaacb86b33
Nick Kulshenko <s3tboy1993@yandex.ru>
2025-02-21 15:56:15 +0300

    New tests for price

--- src/test/java/BunTests.java
+++ src/test/java/BunTests.java
@@ -13,7 +13,7 @@ public class BunTests {
     @Test
     public void getNameIsSuccessTest() {
         String bunName = "Космическая булка";
-        Bun bun = new Bun(bunName, 100);
+        Bun bun = new Bun(bunName, 100f);
         MatcherAssert.assertThat("Неверное название булки",
                 bun.getName(),
                 equalTo(bunName));
@@ -23,7 +23,37 @@ public class BunTests {
     //  !--- Проверяем метод на запрос цены булки ---!
     @Test
     public void getPriceIsSuccessTest() {
-        float bunPrice = 100;
+        float bunPrice = 100f;
+        Bun bun = new Bun("Космическая булка", bunPrice);
+        MatcherAssert.assertThat("Неверная цена булки",
+                bun.getPrice(),
+                equalTo(bunPrice));
+    }
+
+    //  !--- Проверяем метод на запрос нулевой цены булки ---!
+    @Test
+    public void getZeroPriceIsSuccessTest() {
+        float bunPrice = 0f;
+        Bun bun = new Bun("Космическая булка", bunPrice);
+        MatcherAssert.assertThat("Неверная цена булки",
+                bun.getPrice(),
+                equalTo(bunPrice));
+    }
+
+    //  !--- Проверяем метод на запрос отрицательной цены булки ---!
+    @Test
+    public void getNegativePriceIsSuccessTest() {
+        float bunPrice = -100f;
+        Bun bun = new Bun("Космическая булка", bunPrice);
+        MatcherAssert.assertThat("Неверная цена булки",
+                bun.getPrice(),
+                equalTo(bunPrice));
+    }
+
+    //  !--- Проверяем метод на запрос дробной цены булки ---!
+    @Test
+    public void getFractionalPriceIsSuccessTest() {
+        float bunPrice = 100.15f;
         Bun bun = new Bun("Космическая булка", bunPrice);
         MatcherAssert.assertThat("Неверная цена булки",
                 bun.getPrice(),
