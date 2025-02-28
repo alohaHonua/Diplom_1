05915425de47e9849d55a4dcb7c2e0863f2b6ea4
Rail <rail.tehs@gmail.com>
2025-02-28 17:18:50 +0400

    Add unit tests for all classes

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
 
 public class BunTest {
+
+    // проверка метода, возвращающего имя
+    @Test
+    public void bunNameTest() {
+        Bun bun = new Bun("Classic", 10);
+        assertEquals("Classic", bun.name);
+    }
+
+    // проверка метода, возвращающего стоимость
+    @Test
+    public void bunPriceTest() {
+        Bun bun = new Bun("Zinger", 11);
+        assertEquals(11, bun.getPrice(), 0.001);
+    }
 }
