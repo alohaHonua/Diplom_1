bc71f33f92ebecebf0c51d41cd573084267c93b0
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-03-10 19:53:56 +0700

    BunTest Complete#2

--- src/test/java/BunParameterizedTests.java
+++ src/test/java/BunParameterizedTests.java
@@ -7,36 +7,36 @@ import praktikum.Bun;
 @RunWith(Parameterized.class)
 public class BunParameterizedTests {
     private final String name;
-    private final String expectedName;
     private final float price;
-    private final float expectedPrice;
 
-    public BunParameterizedTests(String name, String expectedName, float price, float expectedPrice){
+    public BunParameterizedTests(String name, float price){
         this.name = name;
-        this.expectedName = expectedName;
         this.price = price;
-        this.expectedPrice = price;
     }
 
     @Parameterized.Parameters
     public static Object[][] bunData() {
         return new Object[][] {
-                {"Флюоресцентная булка R2-D2", "Флюоресцентная булка R2-D2", 988f, 988f},
-                {"Краторная булка N-200i", "Краторная булка N-200i", 1255f, 1255f},
-                {"Булка как булка", "Булка как булка", 500.250f, 500.250f},
+                {"Флюоресцентная булка R2-D2", 988f},
+                {"Краторная булка N-200i", 1255f},
+                {"Булка как булка", 500.250f},
         };
     }
 
     @Test
     public void bunNameTest(){
-        Bun bun = new Bun(name, price);
-        Assert.assertEquals(expectedName, bun.getName());
+        Bun bun = createBun();
+        Assert.assertEquals(name, bun.getName());
     }
 
     @Test
     public void bunPriceTest(){
-        Bun bun = new Bun(name, price);
-        Assert.assertEquals(expectedPrice, bun.price, 0);
+        Bun bun = createBun();
+        Assert.assertEquals(price, bun.price, 0);
+    }
+
+    private Bun createBun(){
+        return new Bun(name, price);
     }
 
 }
