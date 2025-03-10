af264e4f6914e699a65e060db658ffef3d7cecb1
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-03-10 19:46:10 +0700

    IngredientTest Complete#2

--- src/test/java/IngredientParameterizedTests.java
+++ src/test/java/IngredientParameterizedTests.java
@@ -8,46 +8,44 @@ import praktikum.IngredientType;
 @RunWith(Parameterized.class)
 public class IngredientParameterizedTests {
     private final IngredientType type;
-    private final IngredientType expectedType;
     private final String name;
-    private final String expectedName;
     private final float price;
-    private final float expectedPrice;
 
-    public IngredientParameterizedTests(IngredientType type, IngredientType expectedType, String name, String expectedName, float price, float expectedPrice){
+    public IngredientParameterizedTests(IngredientType type, String name, float price){
         this.type = type;
-        this.expectedType = expectedType;
         this.name = name;
-        this.expectedName = expectedName;
         this.price = price;
-        this.expectedPrice = expectedPrice;
     }
 
     @Parameterized.Parameters
     public static Object[][] ingredientData(){
         return new Object[][]{
-                {IngredientType.SAUCE, IngredientType.SAUCE, "Соус Spicy-X", "Соус Spicy-X", 90f, 90f},
-                {IngredientType.SAUCE, IngredientType.SAUCE, "Соус как соус", "Соус как соус", 88.8888f, 88.8888f},
-                {IngredientType.FILLING, IngredientType.FILLING, "Мясо бессмертных моллюсков Protostomia", "Мясо бессмертных моллюсков Protostomia", 1337f, 1337f},
-                {IngredientType.FILLING, IngredientType.FILLING, "Говяжий метеорит (отбивная)", "Говяжий метеорит (отбивная)", 3000.000f, 3000.000f},
+                {IngredientType.SAUCE, "Соус Spicy-X", 90f},
+                {IngredientType.SAUCE, "Соус как соус", 88.8888f},
+                {IngredientType.FILLING, "Мясо бессмертных моллюсков Protostomia", 1337f},
+                {IngredientType.FILLING, "Говяжий метеорит (отбивная)", 3000.000f},
         };
     }
 
     @Test
     public void getPriceTest(){
-        Ingredient ingredient = new Ingredient(type, name, price);
-        Assert.assertEquals(expectedPrice, ingredient.getPrice(), 0);
+        Ingredient ingredient = createIngredient();
+        Assert.assertEquals(price, ingredient.getPrice(), 0);
     }
 
     @Test
     public void getNameTest(){
-        Ingredient ingredient = new Ingredient(type, name, price);
-        Assert.assertEquals(expectedName, ingredient.getName());
+        Ingredient ingredient = createIngredient();
+        Assert.assertEquals(name, ingredient.getName());
     }
 
     @Test
     public void getTypeTest(){
-        Ingredient ingredient = new Ingredient(type, name, price);
-        Assert.assertEquals(expectedType, ingredient.getType());
+        Ingredient ingredient = createIngredient();
+        Assert.assertEquals(type, ingredient.getType());
+    }
+
+    private Ingredient createIngredient(){
+        return new Ingredient(type, name, price);
     }
 }
