923da6d1cbaa970ebf496b0eb23d1102fb9f6ffd
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-03-14 20:12:22 +0700

    Mock for bun(setBunTest)#2

--- src/test/java/BurgerTests.java
+++ src/test/java/BurgerTests.java
@@ -1,14 +1,18 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
 /*    public static void main(String[] args){
         Bun bun = new Bun("qwerty", 3.14f);
@@ -21,17 +25,25 @@ public class BurgerTests {
     @Mock
     private Bun bun;
 
-    @Before
-    public void setUp(){
-        MockitoAnnotations.openMocks(this);
-    }
 ///////Этим тестом я проверяю корректную работу метода setBuns в классе Burger
     @Test
-    public void setBunTest() {
-        Mockito.when(bun.getName()).thenReturn("Название булочки");
+    public void setBunTestReturnName() {
+        String testValue = "Название булочки";
+        Mockito.when(bun.getName()).thenReturn(testValue);
         Burger burger = new Burger();
         burger.setBuns(bun);
         String result = burger.bun.getName();
-        assertTrue("Результат не является строкой", result instanceof String);
+        assertEquals(testValue, result);
+    }
+
+///////Этим тестом я проверяю корректную работу метода setBuns в классе Burger
+    @Test
+    public void setBunTestReturnPrice(){
+        float testValue = 1300.4f;
+        Mockito.when(bun.getPrice()).thenReturn(testValue);
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        float result = burger.bun.getPrice();
+        assertEquals(testValue, result, 0);
     }
 }
