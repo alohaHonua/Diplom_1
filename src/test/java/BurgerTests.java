94080d2190c664f18a2ff30010ab813173ba2ceb
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-03-15 12:55:28 +0700

    Пытаюсь понять как работает класс Бургер

--- src/test/java/BurgerTests.java
+++ src/test/java/BurgerTests.java
@@ -6,28 +6,26 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import praktikum.Bun;
-import praktikum.Burger;
+import praktikum.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
-/*    public static void main(String[] args){
-        Bun bun = new Bun("qwerty", 3.14f);
-        Burger burger = new Burger();
-        burger.setBuns(bun);
-        System.out.println(burger.bun.price);
-        System.out.println(burger.bun.name);
-    } */
 
     @Mock
     private Bun bun;
 
-///////Этим тестом я проверяю корректную работу метода setBuns в классе Burger
+    @Mock
+    private Ingredient ingredient;
+
+    @Mock
+    private Database database;
+
     @Test
     public void setBunTestReturnName() {
+        //Этим тестом я проверяю корректную работу метода setBuns в классе Burger, если булка установилась можно вернуть её название
         String testValue = "Название булочки";
         Mockito.when(bun.getName()).thenReturn(testValue);
         Burger burger = new Burger();
@@ -36,14 +34,24 @@ public class BurgerTests {
         assertEquals(testValue, result);
     }
 
-///////Этим тестом я проверяю корректную работу метода setBuns в классе Burger
     @Test
     public void setBunTestReturnPrice(){
+        //Этим тестом я проверяю корректную работу метода setBuns в классе Burger, если булка установилась можно вернуть её цену
         float testValue = 1300.4f;
         Mockito.when(bun.getPrice()).thenReturn(testValue);
         Burger burger = new Burger();
         burger.setBuns(bun);
         float result = burger.bun.getPrice();
         assertEquals(testValue, result, 0);
+    }
+
+    @Test
+    public void addIngredientTest(){
+        Burger burger = new Burger();
+        Ingredient ingredient1 = new Ingredient(IngredientType.SAUCE, "соусхуёус", 1.333f);
+        Ingredient ingredient2 = new Ingredient(IngredientType.FILLING, "соусхуёcxcxус", 1.333f);
+        burger.addIngredient(ingredient1);
+        burger.addIngredient(ingredient2);
+        System.out.println(burger.ingredients.size());
     }
 }
