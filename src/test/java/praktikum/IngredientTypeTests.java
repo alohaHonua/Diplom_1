1ecce9af7271a96cd520ed43c72552ae124cd85e
Pavel Sveshnikov <prav-sha@yandex.ru>
2025-03-29 02:00:19 +0700

    add tests for ingredientType

--- src/test/java/praktikum/IngredientTypeTests.java
+++ src/test/java/praktikum/IngredientTypeTests.java
@@ -1,4 +1,21 @@
 package praktikum;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class IngredientTypeTests {
+
+    @Test
+    public void ingredientTypeTestValues() {
+        IngredientType[] types = IngredientType.values();
+        assertEquals(IngredientType.FILLING, types[1]);
+        assertEquals(IngredientType.SAUCE, types[0]);
+    }
+
+    @Test
+    public void ingredientTypeTestLength() {
+        IngredientType[] types = IngredientType.values();
+        assertEquals(2, types.length);
+    }
+
 }
