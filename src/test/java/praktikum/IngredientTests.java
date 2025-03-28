c2f8b19109e3820396e733dadaceebfbbbfc84d8
Pavel Sveshnikov <prav-sha@yandex.ru>
2025-03-29 01:59:03 +0700

    add tests for ingredient

--- src/test/java/praktikum/IngredientTests.java
+++ src/test/java/praktikum/IngredientTests.java
@@ -1,4 +1,46 @@
 package praktikum;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class IngredientTests {
+
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientTests(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][] {
+                {IngredientType.FILLING, "Filling_Name", 10.00f},
+                {IngredientType.SAUCE, "Sauce_Name", 1.00f},
+        };
+    }
+
+    @Test
+    public void getTypeTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(type, ingredient.getType());
+    }
+
+    @Test
+    public void getNameTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(name, ingredient.getName());
+    }
+
+    @Test
+    public void getPriceTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals(price, ingredient.getPrice(), 0.0);
+    }
 }
