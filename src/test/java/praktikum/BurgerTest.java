86479a1fa7131643631461cc4b4c4d4e8c3629b4
Anatoliy764 <tun.anatoly@yandex.kz>
2025-04-06 22:10:37 +0600

    Покрыл тестами класс Burger используя моки, стабы и параметризацию.

---
+++ src/test/java/praktikum/BurgerTest.java
@@ -0,0 +1,131 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RunWith(Parameterized.class)
+public class BurgerTest {
+
+    private static Database mockDatabase = Mockito.mock(Database.class);
+
+    private Burger burger;
+
+    static {
+
+        Mockito.when(mockDatabase.availableBuns())
+                .thenReturn(List.of(
+                        new Bun("black bun", 100),
+                        new Bun("white bun", 200),
+                        new Bun("red bun", 300)
+                ));
+
+        Mockito.when(mockDatabase.availableIngredients())
+                .thenReturn(List.of(
+                        new Ingredient(IngredientType.SAUCE, "hot sauce", 100),
+                        new Ingredient(IngredientType.SAUCE, "sour cream", 200),
+                        new Ingredient(IngredientType.SAUCE, "chili sauce", 300),
+                        new Ingredient(IngredientType.FILLING, "cutlet", 100),
+                        new Ingredient(IngredientType.FILLING, "dinosaur", 200),
+                        new Ingredient(IngredientType.FILLING, "sausage", 300)
+                ));
+    }
+
+    @Mock
+    private Ingredient mockIngredient;
+
+    @Parameterized.Parameters
+    public static Collection burgerDataProvider() {
+        List<Bun> buns = mockDatabase.availableBuns();
+        Map<IngredientType, List<Ingredient>> ingredientsByType = mockDatabase.availableIngredients()
+                .stream()
+                .collect(Collectors.groupingBy(Ingredient::getType));
+
+        Object[][] data = new Object[buns.size()][3];
+        for (int i = 0; i < buns.size(); i++) {
+            Bun bun = buns.get(i);
+            List<Ingredient> sauces = ingredientsByType.get(IngredientType.SAUCE);
+            List<Ingredient> fillings = ingredientsByType.get(IngredientType.FILLING);
+            Ingredient sauce = sauces.get(i);
+            Ingredient filling = fillings.get(i);
+            data[i] = new Object[]{bun, sauce, filling};
+        }
+        return Arrays.asList(data);
+    }
+
+    private Bun bun;
+
+    private Ingredient sauce;
+
+    private Ingredient filling;
+
+    public BurgerTest(Bun bun, Ingredient sauce, Ingredient filling) {
+        this.bun = bun;
+        this.sauce = sauce;
+        this.filling = filling;
+    }
+
+    @Before
+    public void init() {
+        burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(sauce);
+        burger.addIngredient(filling);
+    }
+
+    @Test
+    public void addIngredient() {
+        burger.addIngredient(mockIngredient);
+
+        Assert.assertTrue(burger.ingredients.contains(mockIngredient));
+    }
+
+    @Test
+    public void removeIngredient() {
+
+        Ingredient ingredient = burger.ingredients.get(0);
+
+        burger.removeIngredient(0);
+
+        Assert.assertFalse(burger.ingredients.contains(ingredient));
+    }
+
+    @Test
+    public void moveIngredient() {
+
+        Ingredient ingredient = burger.ingredients.get(0);
+
+        burger.moveIngredient(0, 1);
+
+        Assert.assertTrue(burger.ingredients.get(1).equals(ingredient));
+    }
+
+    @Test
+    public void getPrice() {
+        float expectedPrice = bun.getPrice() * 2 + sauce.getPrice() + filling.getPrice();
+        Assert.assertEquals(expectedPrice, burger.getPrice(), 0.001f);
+    }
+
+    @Test
+    public void getReceipt() {
+        StringBuilder expectedReceipt = new StringBuilder(String.format("(==== %s ====)%n", bun.getName()));
+
+        expectedReceipt.append(String.format("= %s %s =%n", sauce.getType().toString().toLowerCase(),
+                sauce.getName()));
+
+        expectedReceipt.append(String.format("= %s %s =%n", filling.getType().toString().toLowerCase(),
+                filling.getName()));
+
+        expectedReceipt.append(String.format("(==== %s ====)%n", bun.getName()));
+        expectedReceipt.append(String.format("%nPrice: %f%n", bun.getPrice() * 2 + sauce.getPrice() + filling.getPrice()));
+
+        Assert.assertEquals(expectedReceipt.toString(), burger.getReceipt());
+    }
+}
