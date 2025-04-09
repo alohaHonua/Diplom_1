1ed19b9bdfc1293b22bbd6134c4ff1292b8a20b7
Nadja Chumakova <newcccp@yandex.ru>
2025-04-09 09:04:41 +0200

    update after review

--- src/test/java/BurgerTests.java
+++ src/test/java/BurgerTests.java
@@ -1,10 +1,13 @@
-package praktikum;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 
 import static org.junit.Assert.*;
