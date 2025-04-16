ab6d2986a93cbe8ded14c23dde09f9dc823f7b3a
baagafonov <sir.saikom@ya.ru>
2025-04-16 22:02:01 +0300

    added Enum test

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTypeTest {
+
+    @Test
+    public void valueOf() {
+        assertEquals(IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
+    }
+}
