8d0479944158205ea3ab5896d3ff3a56f3812e4f
Vladimir Bratsiun <v.braciun@insitech.dev>
2025-06-05 23:39:56 +0300

    - добавил тесты для класса Burger

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -2,28 +2,26 @@ package praktikum;
 
 import org.junit.Before;
 import org.junit.Test;
+import praktikum.constants.TestData;
 
 import static org.junit.Assert.*;
-public class BunTest {
 
-    private static final String BUN_NAME = "Сладкая булочка";
-    private static final float BUN_PRICE = 99999999999.9f;
-    private static final float DELTA = 0.001f;
+public class BunTest {
 
     private Bun bun;
 
     @Before
     public void setUp() {
-        bun = new Bun(BUN_NAME, BUN_PRICE);
+        bun = new Bun(TestData.BUN_NAME, TestData.BUN_MAX_PRICE);
     }
 
     @Test
     public void testGetName() {
-        assertEquals(BUN_NAME, bun.getName());
+        assertEquals(TestData.BUN_NAME, bun.getName());
     }
 
     @Test
     public void testGetPrice() {
-        assertEquals(BUN_PRICE, bun.getPrice(), DELTA);
+        assertEquals(TestData.BUN_MAX_PRICE, bun.getPrice(), TestData.DELTA);
     }
 }
